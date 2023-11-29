a497cc6801ed2bcf1c6a210b129e654ccf77d675
Álvaro Bacca <a.baccap@uniandes.edu.co>
2023-11-28 22:23:31 -0500

    ValueError por importación sobre elevada

--- places/places/urls.py
+++ places/places/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls import url, include
 from django.views.decorators.csrf import csrf_exempt
 
-from ..places import views
+from . import views
 
 urlpatterns = [
     url(r'^places/', views.PlaceList, name='placeList'),
